c79ffc56155625174173447fda05c348a1c52161
Sebastian <sebastian.barragan71@misena.edu.co>
2022-04-03 19:39:07 -0500

    Se sube el proyecto

---
+++ view/404.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error 404</title>
+    <style>
+        *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+    body{
+        background-color: #efefef;
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+        .container-404{
+            text-align: center;
+            width: 100%;
+            max-width: 700px;
+            background-color: #f3f3f3;
+            padding: 40px 0;
+            box-shadow: 0px 1px 10px ;
+            border-radius: 5px ;
+            line-height: 1.7;
+        }
+        .container-404 figure img{
+            width: 250px;
+            height: 250px;
+        }
+        .container-404 p{
+            font-size: 20px;
+        }
+    </style>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container-404">
+        <figure>
+            <img src="/img/vecteezy_error-404-with-the-cute-golf-mascot_.jpg" alt="">
+        </figure>
+        <p>
+            Algo salio mal. estamos trabajando en ello.
+        </p>
+        <p>
+            Disculpa las molestias. 
+        </p>
+        <button type="button" class="btn btn-outline-primary">Página Pricipal</button>
+    </div>
+</body>
+</html>
